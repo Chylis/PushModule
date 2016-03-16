19926b9c2f34e30008797e822d09b96cf386db9f
Magnus Eriksson <magnus.eriksson@apegroup.com>
2016-03-16 16:56:53 +0100

    Extended request_utils with json parsing

--- src/lib/request_utils.erl
+++ src/lib/request_utils.erl
@@ -5,56 +5,91 @@
 %%% API
 %%%
 
-% Input: 
-% Params = ["body", "title", "user"], 
-% Req = Simple bridge request object with contents: {"body":"hejsan", "user":"maggan"}
-% Output {"hejsan", [], "maggan"}
+% Fetches several params from a json object
+params(Params, Json) when is_list(Json) ->
+  json_params(Params, Json, []);
+
+% Fetches several params from a request object
 params(Params, Req) ->
-  params(Params, Req, []).
+  req_params(Params, Req, []).
 
+% Fetches a single param from a json object
+param(Param, Json) when is_list(Json) ->
+  param_from_json(Param, Json);
+
+% Fetches a single param from a request object
 param(Param, Req) ->
-  evaluate_param(Param, Req).
+  param_from_request(Param, Req).
 
 
-%%% Internal
 
-params([], _Req, Acc) ->
+
+
+%%%
+%%% Request
+%%%
+
+req_params([], _Req, Acc) ->
   erlang:list_to_tuple(lists:reverse(Acc));
-params([Param|T], Req, Acc) ->
-  EvaluatedParam = evaluate_param(Param, Req),
-  params(T, Req, [EvaluatedParam|Acc]).
+req_params([Param|T], Req, Acc) ->
+  EvaluatedParam = param_from_request(Param, Req),
+  req_params(T, Req, [EvaluatedParam|Acc]).
 
 
-% @param Param = "password"
-% @param Req = Simple bridge request object with contents: {"password":"secretpassword123"}
-% @return "secretpassword123" | []
-evaluate_param(Param, Req) ->
-  case json_body_from_request(Req) of
-
-    undefined ->   form_param(Param, Req);
-
-    PropList  ->   
-      case proplists:get_value(Param, PropList) of
-        undefined   -> [];
-        Value       -> Value
-      end
+% Req -> Plist -> Value | 
+% Req -> Body -> Value
+param_from_request(Param, Req) ->
+  case property_list_from_request(Req) of
+    undefined ->   form_param_from_request(Param, Req);
+    PropList  ->   param_from_property_list(Param, PropList)
   end.
 
-% Receives a Simple Bridge request object.
-% Returns the body from the request body as a property list, or undefined.
-json_body_from_request(Req) ->
+% Req -> Plist
+property_list_from_request(Req) ->
   JsonBody = Req:request_body(),
-  try
-    {struct, PropList} = mochijson:decode(JsonBody),
-    PropList
-  catch
-    _:_   -> undefined
-  end.
+  property_list_from_json(JsonBody).
 
-
-form_param(Param, Req) ->
+% Req -> Value
+form_param_from_request(Param, Req) ->
   case Req:post_param(Param) of
     "undefined" -> [];
     Value       -> Value
   end.
 
+
+
+
+
+
+
+%%%
+%%% JSON 
+%%%
+
+json_params([], _Json, Acc) ->
+  erlang:list_to_tuple(lists:reverse(Acc));
+json_params([Param|T], Json, Acc) ->
+  EvaluatedParam = param_from_json(Param, Json),
+  json_params(T, Json, [EvaluatedParam|Acc]).
+
+% Json -> PList -> Property | []
+param_from_json(Param, Json) ->
+  case property_list_from_json(Json) of
+    undefined -> [];
+    PropList -> param_from_property_list(Param, PropList)
+  end.
+
+property_list_from_json(Json) ->
+  try
+    {struct, PropList} = mochijson:decode(Json),
+    PropList
+  catch
+    _:_   -> undefined
+  end.
+
+% Plist -> Property
+param_from_property_list(Param, PropList) ->
+  case proplists:get_value(Param, PropList) of
+    undefined   -> [];
+    Value       -> Value
+  end.
